4488ad7cc656f4c87ae426eb15e9cde93d73339a
Thompson Liu <liu.thompson@gmail.com>
2019-01-21 00:41:04 -0500

    Data Visualization and Updates

---
+++ DrillDescribingDataFunction.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 19 15:06:06 2019
+
+@author: liuth
+"""
+
+import numpy as np
+ages = [14,12,11,10,8,6,8,6,6]
+ages2 = [14,12,11,10,8,7,8]
+ages3 = [14,12,11,10,8,7,1]
+
+def modes(ages):
+
+   # Empty dict we'll use to store item counts.
+   counts = {}
+
+   # Populate the dict.
+   for value in ages:
+       if value in counts:
+           counts[value] += 1
+       else:
+           counts[value] = 1
+
+   # Find the number of times the mode appears.
+   max_occurrence = max(counts.values())
+
+   # Short-circuit if there is no mode.
+   min_occurrence = min(counts.values())
+   if max_occurrence == min_occurrence:
+       return []
+
+   # Initialize an empty list to populate and return.
+   result = []
+   for key in counts.keys():
+       if counts[key] == max_occurrence:
+           result.append(key)
+
+   return print(sorted(result))
+
+
+modes(ages)
---
+++ WhatDoYouSee.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 20 23:55:43 2019
+
+@author: liuth
+
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+
+mydata = pd.read_csv('Admission_Predict.csv')
+mydata.columns = ['SerialNo.', 'GREScore',\
+                  'TOEFLScore', 'UniversityRating', 'SOP',\
+                  'LOR', 'CGPA', 'Research', 'ChanceofAdmit']
+
+#add grids 
+sns.set()
+
+visl = sns.distplot(mydata["GREScore"], bins=5)
+#This is a distribution of all the GRE Scores.
+
+vis2 = sns.lmplot(data=mydata, x='GREScore', y='TOEFLScore',\
+                  fit_reg=False, hue='GREScore', size=8,\
+                  y_jitter=.02, logistic=True)
+#This is a scatter plot showing a distribution of the GREscore in comparison
+#with the TOEFL score
+
+vis3 = sns.boxplot(data=mydata, x='UniversityRating', y='ChanceofAdmit')
+#This shows the university rating comparison with the chances of being admitted
+
+vis4 = sns.violinplot(data=mydata, x='ChanceofAdmit', y='GREScore')
+#Gives a visualization of GRE score and the chance of admit.  Perhaps correlation?
